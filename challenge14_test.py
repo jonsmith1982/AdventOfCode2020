ec6451898c7fea59f637e7f7ebdcc22ec3019a1c
Simon Hodgson <simon.hodgson.1982@gmail.com>
2020-12-14 16:53:24 +0000

    removed undefined var in test

--- challenge14_test.py
+++ challenge14_test.py
@@ -4,9 +4,9 @@ with open("challenge-14-12-2020.txt","r") as f:
   puzzle_input = [x.strip() for x in f]
 
 def test_part1():
-  part1_result = challenge.part1(puzzle_input)
+  part1_result = part1(puzzle_input)
   assert part1_result == 16003257187056
 
 def test_part2():
-  part2_result = challenge.part2(puzzle_input)
+  part2_result = part2(puzzle_input)
   assert part2_result == 3219837697833
