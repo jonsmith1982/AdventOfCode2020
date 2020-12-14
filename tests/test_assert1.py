fb29667f4297fd2daa6d66803f5c56bae71e72c7
Simon Hodgson <simon.hodgson.1982@gmail.com>
2020-12-14 16:06:29 +0000

    Git action test

--- tests/test_assert1.py
+++ tests/test_assert1.py
@@ -66,11 +66,11 @@ def part2(input):
 
   return(sum(memory.values()))
 
-def test_part1(answer):
+def test_part1:
   part1_result = part1(puzzle_input)
   assert part1_result == 16003257187056
 
-def test_part2(answer):
+def test_part2:
   part2_result = part2(puzzle_input)
   assert part2_result == 3219837697833
 
