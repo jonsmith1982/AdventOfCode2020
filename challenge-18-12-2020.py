3aaaf3765c04edf1b1a307ca757152a9264d6146
Simon Hodgson <simon.hodgson.1982@gmail.com>
2020-12-18 22:01:56 +0000

    Had a bash, at day 18.

--- challenge-18-12-2020.py
+++ challenge-18-12-2020.py
@@ -11,6 +11,7 @@ with open("challenge-18-12-2020.txt","r") as f:
   puzzle_input = [x.strip() for x in f]
 
 def part1(input):
+  count = 0
   
   def deparentise(string):
     stack = []
@@ -19,23 +20,36 @@ def part1(input):
         stack.append(i)
       elif c == ')' and stack:
         start = stack.pop()
-        if (re.match('[\(\)]', string[start + 1: i])):
-          deparentise(string[start + 1: i])
-        else :
-          yield (len(stack), string[start + 1: i])
+        yield(string[start + 1: i])
 
-  string = '(9 * 3 + 4) * (7 + (2 + 3 + 6 * 2) + 3) * 4 * 5'
-  sum_of = list(deparentise(string))
-  print(sum_of)
-  
-  for i, s in enumerate(sum_of):
-    if (re.match('[\(\)]', s[1])):
-      print(list(deparentise(s[1])))
+  #string = '(9 * 3 + 4) * (7 + (2 + 3 + 6 * 2) + 3) * 4 * 5'
+  #string = '2 * 3 + (4 * 5)' # 26
+  #string = '5 + (8 * 3 + 9 + 3 * 4 * 3)' # 437
+  #string = '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))' # 12240
+  #string = '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2' # 13632
 
-  #while (re.match('[\(\)]', string)):
-    #breakdown.append(deparentise(string))
+  for x in input:
+
+    while (len(list(deparentise(x))) > 0):
+      original = s = list(deparentise(x))[0]
+      while (re.search("[\+\-\*\/]", s)):
+        equation = re.findall("(\d+)\s*([\+\-\/\*])\s*(\d+)", s)
+        sum_string = ' '.join(equation[0])
+        equals = eval(sum_string)
+        s = re.sub(re.escape(sum_string), str(equals), s)
+      x = re.sub("\(" + re.escape(original) + "\)", str(s), x)
   
-  return(3348222486398)
+    while (re.search("[\+\-\*\/]", x)):
+      s = x
+      equation = re.findall("(\d+)\s*([\+\-\/\*])\s*(\d+)", s)
+      sum_string = ' '.join(equation[0])
+      equals = eval(sum_string)
+      x = re.sub(re.escape(sum_string), str(equals), x)
+    
+    print(x)
+    count += int(x)
+  
+  return(count) # 3348222486398
 
 def part2(input):
 
